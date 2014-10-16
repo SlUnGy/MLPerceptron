d8189ec2db7b8e220b5ab7fd9fd6e9fd3e39813e
SlackUnfairguy <SlackUnfairguy@users.noreply.github.com>
2014-10-16 14:16:28 +0200

    generalized main

--- main.cpp
+++ main.cpp
@@ -4,29 +4,64 @@
 
 int main()
 {
-    MLP bp;
+    constexpr int samples    = 4;
+    constexpr int inputWidth = 2;
 
-    const float p00 [] = {0.0f, 0.0f};
-    const float p01 [] = {0.0f, 1.0f};
-    const float p10 [] = {1.0f, 0.0f};
-    const float p11 [] = {1.0f, 1.0f};
+    const float params[samples][inputWidth] = {{0.0f, 0.0f},{0.0f, 1.0f},{1.0f, 0.0f},{1.0f, 1.0f}};
+    const float targets[samples] = {0.0f, 1.0f, 1.0, 0.0f};
+    const float eta = 0.25f;
 
+    //print the used data
+    std::cout << "input=" << std::endl;
+    for(int i=0; i<samples; ++i)
+    {
+        std::cout << "(";
+        for(int j=0; j<inputWidth-1; ++j)
+        {
+            std::cout << params[i][0] << ",";
+        }
+        std::cout << params[i][inputWidth-1] << ")=" << targets[i] << std::endl;
+    }
+
+    MLP bp(eta, inputWidth, 3 ,1);
     //train the mlp
     const unsigned long iterations = 5000;
     for ( unsigned  long i = 0; i < iterations; ++i )
     {
-
-        bp.train( p00, 0 );
-        bp.train( p01, 1 );
-        bp.train( p10, 1 );
-        bp.train( p11, 0 );
+        for(int i=0; i<samples; ++i)
+        {
+            bp.train(params[i],targets[i]);
+        }
     }
 
     //see if it learned something
-    std::cout << "0,0 = " << bp.run( p00 ) << std::endl;
-    std::cout << "0,1 = " << bp.run( p01 ) << std::endl;
-    std::cout << "1,0 = " << bp.run( p10 ) << std::endl;
-    std::cout << "1,1 = " << bp.run( p11 ) << std::endl;
+    float results[samples];
+    for(int i=0; i<samples; ++i)
+    {
+        float *tmp= bp.run(params[i]);
+        results[i]= tmp[0];
+        delete [] tmp;
+    }
+
+    //calculate mean squared error
+    float error = 0;
+    for(int i=0; i<samples; ++i)
+    {
+        error += (results[i]-targets[i])*(results[i]-targets[i]);
+    }
+    error *= 1.0f/samples;
+
+    std::cout << std::endl << "results= " << std::endl;
+    for(int i=0; i<samples; ++i)
+    {
+        std::cout << "(";
+        for(int j=0; j<inputWidth-1; ++j)
+        {
+            std::cout << params[i][0] << ",";
+        }
+        std::cout << params[i][inputWidth-1] << ")=" << results[i] << std::endl;
+    }
+    std::cout << "MSE  = " << error << std::endl;
 
     return 0;
 }
