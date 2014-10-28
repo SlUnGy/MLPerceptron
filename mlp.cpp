57b32df5dba6009263c484aa6b7ae767b0bacea4
Slack Unfair <SlackUnfairguy@users.noreply.github.com>
2014-10-28 16:35:48 +0100

    made training and running function templates

--- mlp.cpp
+++ mlp.cpp
@@ -1,16 +1,8 @@
 #include "mlp.h"
 
-#include <random>
 #include <ctime>
 
-#include <iostream>
-
-MLP::MLP()
-    :MLP(0.25f, 2, 3, 1)
-{
-
-}
-
+#include <random>
 
 MLP::MLP(const float pEta, const int pInputPerceptrons, const int pHiddenPerceptrons, const int pOutputPerceptrons)
     : m_eta{pEta}, m_hidPerceptrons{pHiddenPerceptrons}, m_inpPerceptrons{pInputPerceptrons}, m_outPerceptrons{pOutputPerceptrons}
@@ -42,101 +34,4 @@ MLP::MLP(const float pEta, const int pInputPerceptrons, const int pHiddenPercept
 			m_outWeights[i][j] = distribution(mt);
 		}
 	}
-}
-
-
-void MLP::train(const float* pIn,const float *pTarget)
-{
-    float hidOutput[m_hidPerceptrons];
-    for(int i=0; i<m_hidPerceptrons; ++i)
-    {
-        //add constant
-        hidOutput[i] = 1*m_hidWeights[0][i];
-        //sum up all inputs*weightings
-        for(int j=1; j<m_inpPerceptrons+1; ++j)
-        {
-            hidOutput[i] += pIn[j-1] * m_hidWeights[j][i];
-        }
-        hidOutput[i] = sigmoid(hidOutput[i]);
-    }
-
-    float output[m_outPerceptrons];
-    for(int i=0; i<m_outPerceptrons; ++i)
-    {
-        output[i] = 1*m_outWeights[0][i];
-        for(int j=1; j<m_hidPerceptrons+1; ++j)
-        {
-            output[i] += hidOutput[j-1] * m_outWeights[j][i];
-        }
-        output[i] = sigmoid(output[i]);
-    }
-
-    //Backpropagation
-    //error calculation
-    float outDelta[m_outPerceptrons];
-    for(int i=0; i<m_outPerceptrons; ++i)
-    {
-        outDelta[i] = output[i]*(1-output[i])*(pTarget[i]-output[i]);
-    }
-
-    //applying delta to reduce error in output layer
-    for(int i=0; i<m_outPerceptrons; ++i)
-    {
-        m_outWeights[0][i] += m_eta*1*outDelta[i];
-        for(int j=0; j<m_hidPerceptrons; ++j)
-        {
-            m_outWeights[j+1][i] += m_eta*hidOutput[j]*outDelta[i];
-        }
-    }
-
-    float hidError[m_hidPerceptrons];
-    for(int i=0; i<m_hidPerceptrons; ++i)
-    {
-        //m_outWeights[i+1][] -> skip the constant coeffecient
-        hidError[i] = hidOutput[i]*(1-hidOutput[i]);
-        float tmpSum = 0;
-        for(int j=0; j<m_outPerceptrons; ++j)
-        {
-            tmpSum += m_outWeights[i+1][j]*outDelta[j];
-        }
-        hidError[i] *= tmpSum;
-    }
-
-    for(int i=0; i<m_hidPerceptrons; ++i)
-    {
-        m_hidWeights[0][i] += m_eta*1*hidError[i];
-        for(int j=0; j<m_inpPerceptrons; ++j)
-        {
-            m_hidWeights[j+1][i] += m_eta*pIn[j]*hidError[i];
-        }
-    }
-}
-
-float* MLP::run(const float *pIn)
-{
-    float hidOutput[m_hidPerceptrons];
-    for(int i=0; i<m_hidPerceptrons; ++i)
-    {
-        //add constant
-        hidOutput[i] = 1*m_hidWeights[0][i];
-        //sum up all inputs*weightings
-        for(int k=1; k<m_inpPerceptrons+1; ++k)
-        {
-            hidOutput[i] += pIn[k-1] * m_hidWeights[k][i];
-        }
-        hidOutput[i] = sigmoid(hidOutput[i]);
-    }
-
-    float* output = new float[m_outPerceptrons]();
-    for(int i=0; i<m_outPerceptrons; ++i)
-    {
-        output[i] = 1*m_outWeights[0][i];
-        for(int j=1; j<m_hidPerceptrons+1; ++j)
-        {
-            output[i] += hidOutput[j-1] * m_outWeights[j][i];
-        }
-        output[i] = sigmoid(output[i]);
-    }
-
-    return output;
 }
--- mlp.h
+++ mlp.h
@@ -2,19 +2,43 @@
 #define MLP_H_INCLUDED
 
 #include <cmath>
+#include <cstdint>
+#include <iostream>
 
 class MLP {
 	public:
-		MLP();
+		MLP():MLP(0.25f, 2, 3, 1){}
 		MLP(const float, const int, const int, const int);
-		~MLP() {};
+		~MLP()
+        {
+            for(int i=0; i<m_hidPerceptrons; ++i)
+            {
+                delete [] m_hidWeights[i];
+            }
+            delete [] m_hidWeights;
 
-		void train(const float*, const float*);
-		float* run(const float*);
 
-		static float sigmoid( const float pNum) {
+            for(int i=0; i<m_outPerceptrons; ++i)
+            {
+                delete [] m_outWeights[i];
+            }
+            delete [] m_outWeights;
+        }
+
+
+		template<typename T> void train(const T*, const T*);
+		template<typename T> T* run(const T*);
+
+        template<typename T> static float sigmoid(const T pNum)
+        {
+            return (1.0f/(1.0f+exp(-pNum)));
+        }
+
+        static float sigmoid( const float pNum )
+        {
             return (1.0f/(1.0f+expf(-pNum)));
         }
+
 
 	protected:
 		const float m_eta;
@@ -27,5 +51,101 @@ class MLP {
 		float **m_outWeights;
     private:
 };
+
+template<typename T> void MLP::train(const T* pIn,const T* pTarget)
+{
+    float hidOutput[m_hidPerceptrons];
+    for(int i=0; i<m_hidPerceptrons; ++i)
+    {
+        //add constant
+        hidOutput[i] = 1*m_hidWeights[0][i];
+        //sum up all inputs*weightings
+        for(int j=1; j<m_inpPerceptrons+1; ++j)
+        {
+            hidOutput[i] += pIn[j-1] * m_hidWeights[j][i];
+        }
+        hidOutput[i] = sigmoid(hidOutput[i]);
+    }
+
+    float output[m_outPerceptrons];
+    for(int i=0; i<m_outPerceptrons; ++i)
+    {
+        output[i] = 1*m_outWeights[0][i];
+        for(int j=1; j<m_hidPerceptrons+1; ++j)
+        {
+            output[i] += hidOutput[j-1] * m_outWeights[j][i];
+        }
+        output[i] = sigmoid(output[i]);
+    }
+
+    //Backpropagation
+    //error calculation
+    float outDelta[m_outPerceptrons];
+    for(int i=0; i<m_outPerceptrons; ++i)
+    {
+        outDelta[i] = output[i]*(1-output[i])*(pTarget[i]-output[i]);
+    }
+
+    //applying delta to reduce error in output layer
+    for(int i=0; i<m_outPerceptrons; ++i)
+    {
+        m_outWeights[0][i] += m_eta*1*outDelta[i];
+        for(int j=0; j<m_hidPerceptrons; ++j)
+        {
+            m_outWeights[j+1][i] += m_eta*hidOutput[j]*outDelta[i];
+        }
+    }
+
+    float hidError[m_hidPerceptrons];
+    for(int i=0; i<m_hidPerceptrons; ++i)
+    {
+        //m_outWeights[i+1][] -> skip the constant coeffecient
+        hidError[i] = hidOutput[i]*(1-hidOutput[i]);
+        float tmpSum = 0;
+        for(int j=0; j<m_outPerceptrons; ++j)
+        {
+            tmpSum += m_outWeights[i+1][j]*outDelta[j];
+        }
+        hidError[i] *= tmpSum;
+    }
+
+    for(int i=0; i<m_hidPerceptrons; ++i)
+    {
+        m_hidWeights[0][i] += m_eta*1*hidError[i];
+        for(int j=0; j<m_inpPerceptrons; ++j)
+        {
+            m_hidWeights[j+1][i] += m_eta*pIn[j]*hidError[i];
+        }
+    }
+}
+
+template<typename T> T* MLP::run(const T *pIn)
+{
+    float hidOutput[m_hidPerceptrons];
+    for(int i=0; i<m_hidPerceptrons; ++i)
+    {
+        //add constant
+        hidOutput[i] = 1*m_hidWeights[0][i];
+        //sum up all inputs*weightings
+        for(int k=1; k<m_inpPerceptrons+1; ++k)
+        {
+            hidOutput[i] += pIn[k-1] * m_hidWeights[k][i];
+        }
+        hidOutput[i] = sigmoid(hidOutput[i]);
+    }
+
+    T* output = new T[m_outPerceptrons]();
+    for(int i=0; i<m_outPerceptrons; ++i)
+    {
+        output[i] = 1*m_outWeights[0][i];
+        for(int j=1; j<m_hidPerceptrons+1; ++j)
+        {
+            output[i] += hidOutput[j-1] * m_outWeights[j][i];
+        }
+        output[i] = sigmoid(output[i]);
+    }
+
+    return output;
+}
 
 #endif // MLP_H_INCLUDED
