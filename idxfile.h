c6d881afd0f62a4461299acd38a4d3151fcfcb3e
Slack Unfair <SlackUnfairguy@users.noreply.github.com>
2014-10-28 13:38:41 +0100

    added getter for dimension values

--- idxfile.h
+++ idxfile.h
@@ -13,6 +13,7 @@ public:
     unsigned int getDimensionNumber() { return m_dimensionNumber; }
 
     uint8_t* getData() { return m_data; }
+    uint32_t* getDimensions() { return m_dimension; }
 protected:
 private:
     uint32_t m_magicNumber;
