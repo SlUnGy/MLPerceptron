da5feb0b496ab0882a6a3b5396b5f54a8f144c59
Slack Unfair <SlackUnfairguy@users.noreply.github.com>
2015-01-23 11:03:21 +0100

    moved memory allocation to constructor

--- mlp.cpp
+++ mlp.cpp
@@ -7,6 +7,18 @@
 MultilayerPerceptron::MultilayerPerceptron(const float pEta, const int pInputPerceptrons, const int pHiddenPerceptrons, const int pOutputPerceptrons)
     : m_eta{pEta}, m_hidPerceptrons{pHiddenPerceptrons}, m_inpPerceptrons{pInputPerceptrons}, m_outPerceptrons{pOutputPerceptrons}
 {
+    //+1 due to constant coefficient, i.e. bias
+	m_hidWeights = new float*[m_inpPerceptrons+1];
+    for (int i=0;i<m_inpPerceptrons+1;i++)
+    {
+        m_hidWeights[i] = new float[m_hidPerceptrons];
+	}
+
+	m_outWeights = new float*[m_hidPerceptrons+1];
+    for (int i=0;i<m_hidPerceptrons+1;i++)
+    {
+        m_outWeights[i] = new float[m_outPerceptrons];
+	}
     randomizeWeights();
 }
 
@@ -19,22 +31,15 @@ void MultilayerPerceptron::randomizeWeights()
 	std::mt19937 mt(time(NULL));
 	std::uniform_real_distribution<> distribution(-1, 1);
 
-    //+1 due to constant coefficient, i.e. bias
-	m_hidWeights = new float*[m_inpPerceptrons+1];
     for (int i=0;i<m_inpPerceptrons+1;i++)
     {
-        m_hidWeights[i] = new float[m_hidPerceptrons];
-
 		for (int j=0;j<m_hidPerceptrons;j++) {
 			m_hidWeights[i][j] = distribution(mt);
 		}
 	}
 
-	m_outWeights = new float*[m_hidPerceptrons+1];
     for (int i=0;i<m_hidPerceptrons+1;i++)
     {
-        m_outWeights[i] = new float[m_outPerceptrons];
-
 		for (int j=0;j<m_outPerceptrons;j++) {
 			m_outWeights[i][j] = distribution(mt);
 		}
