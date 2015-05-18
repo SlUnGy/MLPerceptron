6e99cff0681b7924f9fe8a18c0d214f3073986ba
Slack Unfair <SlackUnfairguy@users.noreply.github.com>
2015-05-18 14:54:26 +0200

    added sequential training to main

--- main.cpp
+++ main.cpp
@@ -57,7 +57,88 @@ float calcCorrect(const float * pClassifications, const std::vector<int> *pTarge
     return -1.0f;
 }
 
-int OCLTest() {
+int parallelOCR(std::vector<float> *pTrainingImages, std::vector<float> *pTrainingClassifications,
+             std::vector<float> *pTestImages, std::vector<int> *pTestClassifications,
+             int &pInputwidth, int &pOutputwidth )
+{
+    std::cout << "constructing opencl perceptron." << std::endl;
+    OpenCLPerceptron oclp(0.025f, pInputwidth, 300, pOutputwidth);
+    float *outputBuffer = new float[pTrainingClassifications->size()];
+
+    std::cout << "setting up opencl." << std::endl;
+    if(oclp.initOpenCL())
+    {
+        std::cout << "initialising opencl buffers." << std::endl;
+        if(oclp.initTraining(pTrainingImages,pTrainingClassifications, pTestImages))
+        {
+            std::cout << "training." << std::endl;
+            unsigned int epoch  = 0;
+            float correctness   = 0.0f;
+            const float target  = 0.95f;
+            while(correctness < target)
+            {
+                ++epoch;
+                oclp.trainAll();
+                oclp.testAll(outputBuffer);
+                correctness = calcCorrect(outputBuffer, pTestClassifications, pOutputwidth);
+
+                std::cout << "correct: " << correctness << " - epoch: " << epoch << std::endl;
+            }
+            delete [] outputBuffer;
+            return 0;
+        }
+        else
+        {
+            delete [] outputBuffer;
+            return 1;
+        }
+    }
+    else
+    {
+        delete [] outputBuffer;
+        return 2;
+    }
+}
+
+int sequentialOCR(std::vector<float> *pTrainingImages, std::vector<float> *pTrainingClassifications,
+                  std::vector<float> *pTestImages, std::vector<int> *pTestClassifications,
+                  int &pInputwidth, int &pOutputwidth )
+{
+    std::cout << "constructing one layered perceptron" << std::endl;
+    OneLayerPerceptron olp(0.025f, pInputwidth, 300, pOutputwidth);
+
+    const unsigned int trainingSamples  = pTrainingImages->size()/pInputwidth;
+    const unsigned int testingSamples   = pTestImages->size()/pInputwidth;
+    float *outputBuffer = new float[pTrainingClassifications->size()];
+
+    unsigned int epoch  = 0;
+    float correctness   = 0.0f;
+    const float target  = 0.95f;
+    while(correctness < target)
+    {
+        ++epoch;
+        for(unsigned int i=0; i<trainingSamples; ++i)
+        {
+            const unsigned int imageOffset = i*pInputwidth;
+            const unsigned int classOffset = i*pOutputwidth;
+            olp.train(pTrainingImages->data()+imageOffset, pTrainingClassifications->data()+classOffset);
+        }
+        for(unsigned int i=0; i<testingSamples; ++i)
+        {
+            const unsigned int imageOffset = i*pInputwidth;
+            const unsigned int classOffset = i*pOutputwidth;
+            olp.classify(pTestImages->data()+imageOffset, outputBuffer+classOffset);
+        }
+        correctness = calcCorrect(outputBuffer, pTestClassifications, pOutputwidth);
+
+        std::cout << "correct: " << correctness << " - epoch: " << epoch << std::endl;
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
     std::vector<float> *trainingData            = nullptr;
     std::vector<float> *trainingClassifications = nullptr;
     std::vector<float> *testingData             = nullptr;
@@ -67,57 +148,20 @@ int OCLTest() {
     int outputWidth = 0;
 
     std::cout << "loading and initialising images." << std::endl;
-    if(loadImageData(&trainingData,&trainingClassifications,&testingData,&testingClassifications,inputWidth,outputWidth))
+    if(loadImageData(&trainingData, &trainingClassifications, &testingData, &testingClassifications, inputWidth, outputWidth))
     {
-        std::cout << "constructing opencl perceptron." << std::endl;
-        OpenCLPerceptron oclp(0.05f, inputWidth, 300, outputWidth);
-        float *outputBuffer = new float[trainingClassifications->size()];
+        int retCode = 0;
+        retCode = parallelOCR(trainingData, trainingClassifications, testingData, testingClassifications, inputWidth, outputWidth);
 
-        std::cout << "setting up opencl." << std::endl;
-        if(oclp.initOpenCL())
-        {
-            std::cout << "initialising opencl buffers." << std::endl;
-            if(oclp.initTraining(trainingData,trainingClassifications, testingData))
-            {
-                std::cout << "training." << std::endl;
-                unsigned int epoch  = 0;
-                float correctness   = 0.0f;
-                const float target  = 0.95f;
-                while(correctness<target)
-                {
-                    ++epoch;
-                    oclp.trainAll();
-                    oclp.testAll(outputBuffer);
-                    correctness = calcCorrect(outputBuffer, testingClassifications, outputWidth);
+        trainingData->clear();
+        trainingClassifications->clear();
+        testingData->clear();
+        testingClassifications->clear();
 
-                    std::cout << "correct: " << correctness << " - epoch: " << epoch << std::endl;
-                }
-                delete [] outputBuffer;
-                trainingData->clear();
-                trainingClassifications->clear();
-                testingData->clear();
-                testingClassifications->clear();
-                return 0;
-            }
-            else
-            {
-                delete [] outputBuffer;
-                return 1;
-            }
-        }
-        else
-        {
-            delete [] outputBuffer;
-            return 2;
-        }
+        return retCode;
     }
     else
     {
         return 3;
     }
-}
-
-int main()
-{
-    return OCLTest();
 }
